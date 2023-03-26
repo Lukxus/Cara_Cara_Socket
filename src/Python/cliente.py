6fd2a53da6d69d6b3798d9f68aec6a6fa22b0d0a
Luiz <luizsaraiva50@gmail.com>
2023-03-26 17:03:03 -0300

    Finalizando conexões

---
+++ src/Python/cliente.py
@@ -0,0 +1,41 @@
+import Front.pygamevisual
+import Objetos.figuras
+import Jogo.main
+import connectionFactory.ClienteTCP
+import socket #importa modulo socket
+
+
+TCP_IP = '192.168.15.17' # endereço IP do servidor 
+TCP_PORTA = 24000      # porta disponibilizada pelo servidor
+TAMANHO_BUFFER = 1024
+
+
+def main():
+
+    # Inicia comunicação
+
+    # Criação de socket TCP do cliente
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Conecta ao servidor em IP e porta especifica 
+    cliente.connect((TCP_IP, TCP_PORTA))
+
+    # Jogo
+
+    #bloco de seleção da sua cara
+    objetos = Objetos.figuras.gerando_caras()
+    escolhido = Front.pygamevisual.escolhaCaras(objetos)
+    #print(escolhido.getCorCabelo())
+    recebe_pergunta = connectionFactory.ClienteTCP.recebe_cliente(cliente)
+    answer = Jogo.main.answer(eval(recebe_pergunta), escolhido)
+    #eval converte uma string em tupla
+    connectionFactory.ClienteTCP.envia_cliente(answer)
+    ask = Jogo.main.ask()
+    connectionFactory.ClienteTCP.envia_cliente(ask)
+    recebe_resp = connectionFactory.ClienteTCP.recebe_cliente(cliente)
+    print(f"A resposta dada foi {bool(recebe_resp)}")
+
+
+    cliente.close() # fecha a conexao
+    
+
+main()
--- src/Python/connectionFactory/ClienteTCP.py
+++ src/Python/connectionFactory/ClienteTCP.py
@@ -1,38 +1,37 @@
-import socket #importa modulo socket
+#import socket #importa modulo socket
 
-TCP_IP = '192.168.100.14' # endereço IP do servidor 
+TCP_IP = '192.168.15.17' # endereço IP do servidor 
 TCP_PORTA = 24000      # porta disponibilizada pelo servidor
 TAMANHO_BUFFER = 1024
 MENSAGEM = ''
 count = 0
 
-while 1:
+def envia_cliente(conexao, valor): #valor tem que ser uma string
     while count == 0:
-        MENSAGEM = input("Digite sua mensagem para o servidor: ")
-
-        # Criação de socket TCP do cliente
+        """# Criação de socket TCP do cliente
         cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         # Conecta ao servidor em IP e porta especifica 
-        cliente.connect((TCP_IP, TCP_PORTA))
+        cliente.connect((TCP_IP, TCP_PORTA))"""
 
         # envia mensagem para servidor 
-        cliente.send(MENSAGEM.encode('UTF-8'))
+        conexao.send(valor.encode('UTF-8'))
 
         # recebe dados do servidor 
-        data, addr = cliente.recvfrom(1024)
+        data, addr = conexao.recvfrom(1024)
         print ("received data:", data)
         
         # fecha conexão com servidor
-        cliente.close()
-        count = 1
+        """conexao.close()
+        count = 1"""
 
+def recebe_cliente(conexao):
     while count == 1:
-        clienteRecebe = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clienteRecebe.bind((TCP_IP, TCP_PORTA))
-        clienteRecebe.listen(1)
+        """clienteRecebe = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clienteRecebe.bind((TCP_IP, TCP_PORTA))"""
+        conexao.listen(1)
         # Aceita conexão 
-        conn, addr1 = clienteRecebe.accept()
+        conn, addr1 = conexao.accept()
         print ('Endereço conectado:', addr1)
         #dados retidados da mensagem recebida
         info = conn.recv(TAMANHO_BUFFER)
@@ -40,5 +39,5 @@ while 1:
             print ("Mensagem recebida:", info)
             conn.send(info.upper())
 
-        clienteRecebe.close()
-        count = 0
+        """conexao.close()
+        count = 0"""
